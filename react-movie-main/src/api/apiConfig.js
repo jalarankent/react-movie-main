e9dec96df867c8b0d008d93ede38e43bf4c3908e
jalarankent <147384951+jalarankent@users.noreply.github.com>
2024-08-30 06:41:53 +0000

    movie

--- react-movie-main/src/api/apiConfig.js
+++ react-movie-main/src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'get from themoviedb.org',
+    apiKey: '0f19637d3406db578f27dd82ddb39776',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
